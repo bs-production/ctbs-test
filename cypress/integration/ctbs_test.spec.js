42b9a7bd75fae6ac8d943b0716f8aefd048f2a05
John Siwicki <johnsiwicki@gmail.com>
2020-07-30 15:54:55 -0400

    Update ctbs_test.spec.js

--- cypress/integration/ctbs_test.spec.js
+++ cypress/integration/ctbs_test.spec.js
@@ -1,7 +1,7 @@
 describe('CTBS head and H1 test', () => {
   it('Check for title, description, H1', () => {
    cy.visit('https://www.connecticutbasementsystems.com');
-   cy.title().should('eq', 'Basement Waterproofing in CT | Basement Finishing, Foundation Repair in CT, Westchester, NY, Stamford, Norwalk, West Hartford, Danbury')
+   cy.title().should('eq', 'Basement Waterproofing in CT')
    cy.get('h1').should('be.visible')
    cy.get('head meta[name="description"]')
    .should("have.attr", "content", "Connecticut Basement Systems waterproofs wet basements and crawl spaces. Get our Free cost estimates in Stamford, Norwalk, West Hartford and the nearby areas.");
