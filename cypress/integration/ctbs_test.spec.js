b14d34627f275ac119950fdba1651890ba7e5d5f
John Siwicki <johnsiwicki@gmail.com>
2020-08-04 11:22:42 -0400

    Update ctbs_test.spec.js

--- cypress/integration/ctbs_test.spec.js
+++ cypress/integration/ctbs_test.spec.js
@@ -1,15 +1,16 @@
+const siteName = "https://www.connecticutbasementsystems.com";
+
 describe('CTBS head and H1 test', () => {
   it('Check for title, description, H1', () => {
-   cy.visit('https://www.connecticutbasementsystems.com');
+   cy.visit(siteName);
    cy.get('h1').should('be.visible');
-   cy.get('head meta[name="description"]').should('be.visible');
   })
 
 })
 
 describe('Basic Data Layer Checks', () => {
   it('has a dataLayer and loads GTM', () => {
-    cy.visit('https://www.connecticutbasementsystems.com')
+    cy.visit(siteName)
     cy.window().then((window) => {
       assert.isDefined(window.dataLayer, 
         'window.dataLayer is defined');
